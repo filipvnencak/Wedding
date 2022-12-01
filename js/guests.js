797012a4b647c83e05dc1be85de87d873200381a
filipvnencak <vnencak.filip@gmail.com>
2022-12-01 19:12:47 +0100

    AutoComplete

--- js/guests.js
+++ js/guests.js
@@ -19,32 +19,15 @@ fetch("../guests.json")
     renderButtons(table2, "center");
     renderButtons(table1, "left");
     renderButtons(mainTable, "main");
-    let arrayofPeople = [];
+    const arrayofPeople = [];
     for (let i in json) {
-      arrayofPeople = i.name;
+      arrayofPeople.push(json[i].name);
     }
-    function autocompleteMatch(input) {
-      if (input == "") {
-        return [];
-      }
-      var reg = new RegExp(input);
-      return arrayofPeople.filter(function (term) {
-        if (term.match(reg)) {
-          return term;
-        }
+    $(function () {
+      $("#finder").autocomplete({
+        source: arrayofPeople,
       });
-    }
-
-    function showResults(val) {
-      res = document.getElementById("result");
-      res.innerHTML = "";
-      let list = "";
-      let terms = autocompleteMatch(val);
-      for (i = 0; i < terms.length; i++) {
-        list += "<li>" + terms[i] + "</li>";
-      }
-      res.innerHTML = "<ul>" + list + "</ul>";
-    }
+    });
   });
 
 var form = document.getElementById("form");
